5912382e8b891d8a21c97eb6489070f9c7d86c2e
AmanShukla-creator <amanrms4983@gmail.com>
2024-10-08 16:46:39 +0530

    Update practice.java

--- Loops/practice.java
+++ Loops/practice.java
@@ -45,25 +45,25 @@ public class practice {
         // }
         // System.out.println();
         // }
-        // int n = 4;
-        // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= n - i; j++) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-        int n = 5;
+        int n = 4;
         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
-
-                System.out.print(j + " ");
+                System.out.print("*");
             }
             System.out.println();
-
         }
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+
+        // }
 
     }
 }
