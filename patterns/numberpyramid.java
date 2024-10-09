23c95047c6a73d2ddb115becce4333c742253123
AmanShukla-creator <amanrms4983@gmail.com>
2024-10-09 13:32:46 +0530

    commit

--- patterns/numberpyramid.java
+++ patterns/numberpyramid.java
@@ -8,7 +8,7 @@ public class numberpyramid {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+                System.out.print(i + " ");
             }
             // for (int j = 1; j <= n - i; j++) {
             // System.out.print(" " + " ");
