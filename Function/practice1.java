38debe8f67913836b9028a24a288c8f3f62e74a4
AmanShukla-creator <amanrms4983@gmail.com>
2024-10-09 22:49:21 +0530

    commit

---
+++ Function/practice1.java
@@ -0,0 +1,81 @@
+package Function;
+
+public class practice1 {
+    // public static int printAverage(int a, int b, int c) {
+    // int average = (a + b + c) / 3;
+    // return average;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int c = sc.nextInt();
+    // int average = printAverage(a, b, c);
+    // System.out.println("average of 3 number is :" + average);
+    // }
+    // public static void printSumOdd(int n) {
+    // int a = 0;
+    // for (int i = 1; i <= n; i++) {
+    // if (i % 2 == 0) {
+    // a = a + i;
+    // }
+    // }
+    // System.out.println(a);
+    // return;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int n = sc.nextInt();
+    // printSumOdd(n);
+
+    // }
+    // public static int printGreator(int a, int b) {
+    // if (a > b) {
+    // System.out.println("a is greator");
+    // return a;
+    // } else {
+    // System.out.println("b is greator");
+    // return b;
+    // }
+
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // printGreator(a, b);
+    // }
+    // public static double radius(int r) {
+    // double circumference = 2 * 3.14 * r;
+    // System.out.println(circumference);
+    // return circumference;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int r = sc.nextInt();
+    // radius(r);
+    // }
+    // public static int numbers(int x, int n) {
+    // int a = 1;
+    // for (int i = 0; i <= n; i++)
+    // a = a * x;
+    // System.out.println(a);
+    // return a;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int x = sc.nextInt();
+    // int n = sc.nextInt();
+    // numbers(x, n);
+    // }
+    // public static void main(String[] args) {
+    // do {int i
+
+    // } while (true);
+    // }
+}
